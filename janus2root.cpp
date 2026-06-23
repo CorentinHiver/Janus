f610f26efa275db304f6371b4251fed497f700f8
CorentinHiver <corentin.hiver@free.fr>
2026-06-23 13:53:24 +0200

    v 1.1.0_p2

--- janus2root.cpp
+++ janus2root.cpp
@@ -7,30 +7,21 @@ using namespace Colib;
 
 int main(int argc, char** argv)
 {
-  std::string filename;
-  if (argc == 2) filename = argv[1];
-  else {error("No filename !!"); return 1;}
-
-  // // MyDataReader data(filename); // First version
-  // // data.toRoot();
-
-  Reader5052<64> reader(filename); // Second version
-  // reader.setMaxHits(1e6);
-  auto rootFilename = removeExtension(removePath(filename))+".root";
-  reader.initRootOutput(rootFilename, "recreate");
-  Colib::pauseDebug();
-  while(reader.readEvent()) 
+  if (argc < 2) {error("No filename !!"); return 1;}
+  for (int i = 1; i<argc; ++i)
   {
-    reader.fillTree();
-    debug(reader.getHit());
-    debug("hit cursor", reader.getCursor());
-    // if (reader.getCursor() % 10000 == 0) print("\r", reader.getSizeReadRel());
+    Reader5052<64> reader(argv[i]);
+    auto rootFilename = removeExtension(removePath(argv[i]))+".root";
+    reader.initRootOutput(rootFilename, "recreate");
     Colib::pauseDebug();
+    while(reader.readEvent()) 
+    {
+      reader.fillTree();
+      debug(reader.getHit());
+      debug("hit cursor", reader.getCursor());
+      Colib::pauseDebug();
+    }
+    reader.write();
   }
-  reader.write();
-  // reader.write(rootFilename,"recreate");
-
-  // JanusSession session; // Other project : 
-  // session.connect();
   return 0;
 }
